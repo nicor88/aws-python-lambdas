c96620d82fd78564272e2c9c84c3f3362a740425
nicor88 <ncorda@babbel.com>
2017-06-20 17:56:43 +0200

    removed not needed libs for read_yml

--- src/read_yaml/lambda_function.py
+++ src/read_yaml/lambda_function.py
@@ -1,13 +1,10 @@
 import yaml
-import numpy as np
-# import pandas as pd
 
 print('Loading function')
 
 
 def lambda_handler(event, context):
-    # print(pd.__version__)
-    print(np.__version__)
+    # loading yaml
     with open('config.yml', 'r') as stream:
         data_loaded = yaml.load(stream)
     print(data_loaded)
