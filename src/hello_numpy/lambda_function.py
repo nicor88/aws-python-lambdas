a361be7fe7dea10af11ffb2ac1ac18bc2b55f926
nicor88 <ncorda@babbel.com>
2017-06-20 17:47:37 +0200

    fix path

--- src/hello_numpy/lambda_function.py
+++ src/hello_numpy/lambda_function.py
@@ -10,7 +10,7 @@ logger.info('Loading function')
 
 
 def lambda_handler(event, context):
-    logger.info(np.__version__)
+    logger.info('numpy version is {}'.format(np.__version__))
     # lib_version = {'numpy': np.__version__, 'pandas': pd.__version__}
     lib_version = {'numpy': np.__version__}
     return lib_version
