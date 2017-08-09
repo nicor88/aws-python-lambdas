34ae7648ffd8acad51505f62c9e8a948d97ea3d5
nicor88 <ncorda@babbel.com>
2017-08-09 12:58:52 +0200

    added a version of the lambda to manage the new line

--- src/deliver_to_firehose/lambda_function.py
+++ src/deliver_to_firehose/lambda_function.py
@@ -1,23 +1,53 @@
 import base64
-import boto3
 import botocore.exceptions
 import os
+import boto3
+import logging
+import json
 
-print('Loading function')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
-    firehose = boto3.client('firehose')
-    records = event['Records']
-    records_for_firehose = [{'Data': base64.b64decode(r['kinesis']['data'])} for r in records]
+    # config lambda variables
+    logger.info(event)
+    add_newline = os.environ['ADD_NEWLINE']
     delivery_stream = os.environ['DELIVERY_STREAM']
-    print('Putting {} records to {}'.format(len(records_for_firehose), delivery_stream))
+
+    firehose = boto3.client('firehose')
+
+    records = event['Records']
 
     res = None
-    try:
-        res = firehose.put_record_batch(DeliveryStreamName=delivery_stream,
-                                        Records=records_for_firehose)
-    except botocore.exceptions.ClientError as err:
-        message = err.response['Error']['Message']
-        res = message
+    if add_newline == 'True':
+        records_acc = []
+        for r in records:
+            record = json.loads(base64.b64decode(r['kinesis']['data']).decode())
+            record['sequenceNumber'] = r['kinesis']['sequenceNumber']
+            data_with_separator = str(record) + '\n'
+            logger.info(data_with_separator.encode())
+            record_for_firehose = {'Data': data_with_separator.encode()}
+            try:
+                res = firehose.put_record(DeliveryStreamName=delivery_stream,
+                                          Record=record_for_firehose)
+                records_acc.append(record_for_firehose)
+            except botocore.exceptions.ClientError as err:
+                message = err.response['Error']['Message']
+                res = message
+                logger.error(res)
+
+        logger.info('{} Records were put to {}'.format(len(records_acc),
+                                                       delivery_stream))
+    else:
+        records_for_firehose = [{'Data': base64.b64decode(r['kinesis']['data'])} for r in
+                                records]
+        logger.debug('Putting {} records to {}'.format(len(records_for_firehose), delivery_stream))
+        try:
+            res = firehose.put_record_batch(DeliveryStreamName=delivery_stream,
+                                            Records=records_for_firehose)
+        except botocore.exceptions.ClientError as err:
+            message = err.response['Error']['Message']
+            res = message
+            logger.error(res)
     return res
